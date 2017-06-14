c6f945d71ac0d37612b65b60c131e6b8888f7e71
Carl Boettiger <cboettig@gmail.com>
2017-06-14 03:14:01 +0000

    add draft vignette on creating ropensci records, intended as a guide to package authors or interns adding this metadata to existing packages.

--- tests/testthat/test-datasauce.R
+++ tests/testthat/test-datasauce.R
@@ -2,6 +2,21 @@ testthat::context("test datasauce")
 
 library("datasauce")
 
-testthat::test_that("we can call datasauce",{
+testthat::test_that(
+  
+  "we can call datasauce",
+  {
+    
   datasauce()
-})
+    
+  })
+
+testthat::test_that(
+  "we can validate an example file",
+  {
+    f <- system.file("extdata/example.json", package = "datasauce")
+    compact <- jsonld::jsonld_compact(f, "http://schema.org")
+    
+    
+  }
+)
