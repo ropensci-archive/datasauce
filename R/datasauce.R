df662e6fcf6bb6df1387e70c8ade0e9fc46cf282
steven2249 <stevenysw@berkeley.edu>
2017-10-07 02:17:26 -0700

    Update datasauce.R

--- R/datasauce.R
+++ R/datasauce.R
@@ -1,10 +1,10 @@
-#' datasauce
-#' 
-#' create a datasauce object
-#' 
-#' @importFrom jsonlite write_json
-datasauce <- function(){
-  obj <- list()
-  class(obj) <- "datasauce"
-  obj
-}
+##' Create a validator that can validate json files.
+##' 
+##' @title Create a json validator
+##' @param schema Contents of the json schema, or a filename
+##'   containing a schema
+##' @export
+data_validate <- function(schema){
+  out <- tryCatch(json_validate(schema, schema), error = function(e) e)
+  !any(class(out) == "error")}
+                  
