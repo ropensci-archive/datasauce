aaa85e5ad49718a6c267896f0743cb084c8d8fbb
steven2249 <stevenysw@berkeley.edu>
2017-10-26 20:32:28 -0700

    Update test-datasauce.R

--- tests/testthat/test-datasauce.R
+++ tests/testthat/test-datasauce.R
@@ -8,14 +8,6 @@ testthat::test_that(
   {
     f <- system.file("extdata/example.json", package = "datasauce")
     data_validate(f)
-  })
-
-testthat::test_that(
-  "we can validate an example file",
-  {
-    f <- system.file("extdata/example.json", package = "datasauce")
-    compact <- jsonld::jsonld_compact(f, "http://schema.org")
-    
-    
+  }) 
   }
 )
