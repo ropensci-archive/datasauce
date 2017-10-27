d19daaba3474997af692c1c2b8b916f5fd3ecdde
steven2249 <stevenysw@berkeley.edu>
2017-10-26 22:22:21 -0700

    Update data_validate.R

--- R/data_validate.R
+++ R/data_validate.R
@@ -1,10 +1,3 @@
-##' Create a validator that can validate json files.
-##' 
-##' @title Create a json validator
-##' @param schema Contents of the json schema, or a filename
-##'   containing a schema
-##' @export
-
 data_validate <- function(json){
     schema <- system.file("extdata/schema.json", package = "datasauce")
     jsonvalidate::json_validate(json, schema, verbose = TRUE)
