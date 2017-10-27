8ae219de2075a4da4b8ee4eafe41e5a7ce067b70
steven2249 <stevenysw@berkeley.edu>
2017-10-26 23:14:57 -0700

    Update data_validate.R

--- R/data_validate.R
+++ R/data_validate.R
@@ -1,9 +1,3 @@
-\name{data_validate}
-\alias{data_validate}
-\alias{datasauce}
-\title{Create a json validator for dataset.}
-\description{\code{data_validate} returns whether a json is valid for dataset.}
-\usage{data_validate(json)}
 
 data_validate <- function(json){
     schema <- system.file("extdata/schema.json", package = "datasauce")
