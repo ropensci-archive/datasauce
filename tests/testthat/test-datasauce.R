c445a2396234b3e106077635f761f58450474f2a
Carl Boettiger <cboettig@gmail.com>
2018-05-17 18:20:00 -0700

    basic tests

---
+++ tests/testthat/test-datasauce.R
@@ -0,0 +1,9 @@
+
+
+
+ds <- datasauce:::get_schemas("Dataset")
+ds$Dataset
+dataset <- datasauce:::template_constructor("Dataset")
+ds <- dataset(name = "My dataset")
+datasauce:::get_classes()
+datasauce:::get_properties()
